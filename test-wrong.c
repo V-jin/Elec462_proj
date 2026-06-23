5fdaede4013473dbdf639810cd72ebc7612d4526
V-Jin <nice_apple@naver.com>
2026-06-24 03:30:39 +0900

    upload all file

---
+++ test-wrong.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int n, num, sum = 0;
+    if (scanf("%d", &n) != 1) return 0;
+    for (int i = 0; i < n; i++) {
+        if (scanf("%d", &num) == 1) sum += num;
+    }
+    printf("%d\n", sum + 1); // 틀린 코드
+    return 0;
+}
